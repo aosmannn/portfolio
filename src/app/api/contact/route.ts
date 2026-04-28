f6c19d2c86e1bb46b26b13e2331219b5d9ee7602
Adam Osman <kawaiiyou3@gmail.com>
2026-04-28 14:57:00 -0400

    Fix contact from address to adamosman.dev domain

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
   }
   try {
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'Portfolio <noreply@adamosman.dev>',
       to: 'adamosmn06@gmail.com',
       subject: `Portfolio contact from ${name}`,
       text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
